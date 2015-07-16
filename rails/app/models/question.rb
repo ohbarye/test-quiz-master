94934d3eaf267ac463f973435dd85b9a45cc64d7
ohbarye <over.rye@gmail.com>
2015-07-16 23:03:15 +0900

    Implement Question.is_correct? method so that tests pass.

--- rails/app/models/question.rb
+++ rails/app/models/question.rb
@@ -4,7 +4,23 @@ class Question < ActiveRecord::Base
   validates_presence_of :answer
 
   def is_correct?(submission)
-    answer == submission
+    normalize(self.answer) == normalize(submission)
+  end
+
+  private
+
+  def normalize(str)
+    number_to_word(
+      remove_extra_white_spaces(str.downcase())
+    )
+  end
+
+  def number_to_word(str)
+    str.gsub(/\d/) { |num| num.to_i.to_words }
+  end
+
+  def remove_extra_white_spaces(str)
+    str.strip.squeeze(' ')
   end
 
 end
